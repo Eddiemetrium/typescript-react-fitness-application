fa727f14c384634d24fcce8155271a62266e93c2
eddiemetrium <eddiemitch262@gmail.com>
2023-04-02 01:28:31 +0300

    navbar complete but not clickable

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import NavBar from "@/scenes/navbar"
+import { useState,useEffect } from "react"
+import { SelectedPage } from "./shared/types";
+
+function App() {
+const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home) 
+const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+useEffect(()=>{
+const handleScroll = () => {
+  if (window.scrollY === 0){
+    setIsTopOfPage(true)
+    setSelectedPage(SelectedPage.Home)
+  }
+  if (window.scrollY !== 0) setIsTopOfPage(false);
+} 
+window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+},[]) 
+
+  return (
+    <div className="app">
+<NavBar
+
+isTopOfPage={isTopOfPage}
+
+selectedPage={selectedPage}
+    setSelectedPage={setSelectedPage} />  
+    </div>
+  )
+}
+
+export default App
